51f9e53bfe090d6a1ea0cb07d98320d4c6d12830
animation <763678968@qq.com>
2019-08-10 12:07:32 +0800

    Initial commit

---
+++ src/handler/SpringMVCHandler.java
@@ -0,0 +1,13 @@
+package handler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpringMVCHandler {
+
+    @RequestMapping("welcome")
+    public String welcome() {
+        return "success";
+    }
+}
