fbd997c1a57bedb629d36928ed3f970bc60655e2
animation <763678968@qq.com>
2019-08-12 21:19:08 +0800

    使用对象接收请求参数 创建Address Studetn类

---
+++ src/entity/Address.java
@@ -0,0 +1,22 @@
+package entity;
+
+public class Address {
+    private String homeAddresss;
+    private String SchoolAddress;
+
+    public String getHomeAddresss() {
+        return homeAddresss;
+    }
+
+    public void setHomeAddresss(String homeAddresss) {
+        this.homeAddresss = homeAddresss;
+    }
+
+    public String getSchoolAddress() {
+        return SchoolAddress;
+    }
+
+    public void setSchoolAddress(String schoolAddress) {
+        SchoolAddress = schoolAddress;
+    }
+}
