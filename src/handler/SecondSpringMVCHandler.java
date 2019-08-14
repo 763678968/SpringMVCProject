c8301afbd36a209e01a603f2dc7cb7503e370a64
animation <763678968@qq.com>
2019-08-14 17:16:32 +0800

    异常处理 @ExceptionHandler注解

---
+++ src/handler/SecondSpringMVCHandler.java
@@ -0,0 +1,37 @@
+package handler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("second")
+public class SecondSpringMVCHandler {
+    @RequestMapping("testExceptionHandler")
+    public String testExceptionHandler() {
+//        try {
+        System.out.println(1 / 0);
+//        } catch(ArithmeticException e)
+        return "success";
+    }
+
+    @RequestMapping("testExceptionHandler2")
+    public String testExceptionHandler2() {
+        int[] nums = new int[2];
+        System.out.println(nums[2]);
+        return "success";
+    }
+
+    // 该方法可以捕获本类中抛出的ArithmeticException异常
+    @ExceptionHandler({ArithmeticException.class, ArrayIndexOutOfBoundsException.class})
+    public ModelAndView handlerArithmeticException(Exception e) {
+        ModelAndView mv = new ModelAndView("error");
+        System.out.println(e + "==========");
+        mv.addObject("er", e);
+        return mv;
+    }
+
+}
