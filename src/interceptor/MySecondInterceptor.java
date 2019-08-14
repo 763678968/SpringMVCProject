0b5be559846cc13cd516d1ac282f22b6c585ad63
animation <763678968@qq.com>
2019-08-14 16:48:57 +0800

    第二个拦截器

--- src/interceptor/MySecondInterceptor.java
+++ src/interceptor/MySecondInterceptor.java
@@ -6,20 +6,20 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MyInterceptor implements HandlerInterceptor {
+public class MySecondInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("拦截请求"); //
-        return true; // true：拦截操作之后，放行；false：拦截之后不放行，请求终止
+        System.out.println("第二个拦截器，拦截请求....222");
+        return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("拦截响应");
+        System.out.println("第二个拦截器，拦截响应....222");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println("视图(jsp)被渲染完毕");
+        System.out.println("第二个拦截器，afterCompletion....222");
     }
 }
