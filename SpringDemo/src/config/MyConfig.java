3def531f4c8dd1dd0654097773e84a98d9ad5f6a
animation <763678968@qq.com>
2019-10-02 17:24:38 +0800

    IOC容器 注解 MyConfig.java配置类 存Bean

--- SpringDemo/src/config/MyConfig.java
+++ SpringDemo/src/config/MyConfig.java
@@ -1,7 +1,17 @@
 package config;
 
+import entity.Address;
+import entity.Student;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyConfig {
+
+    @Bean // id="myStudent", id默认就是方法名
+    public Student myStudent() {
+        Student student = new Student(100, "zs100", 88);
+        student.setAddress(new Address("xa", "bj"));
+        return student;
+    }
 }
