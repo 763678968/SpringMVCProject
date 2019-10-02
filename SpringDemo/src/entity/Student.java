b3281b968dc7ad17213e45618988464e4a7d60e7
animation <763678968@qq.com>
2019-10-02 17:05:33 +0800

    IOC容器

---
+++ SpringDemo/src/entity/Student.java
@@ -0,0 +1,56 @@
+package entity;
+
+import java.util.Date;
+
+public class Student {
+
+    // 简单类型：8个基本类型+String
+    private int stuNo;
+    private String stuName;
+    private int stuAge;
+
+    // 非简单类型
+    private Address address;
+
+    public int getStuNo() {
+        return stuNo;
+    }
+
+    public void setStuNo(int stuNo) {
+        this.stuNo = stuNo;
+    }
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public int getStuAge() {
+        return stuAge;
+    }
+
+    public void setStuAge(int stuAge) {
+        this.stuAge = stuAge;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "stuNo=" + stuNo +
+                ", stuName='" + stuName + '\'' +
+                ", stuAge=" + stuAge +
+                ", address=" + address +
+                '}';
+    }
+}
